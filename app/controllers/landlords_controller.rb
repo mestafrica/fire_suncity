8341b930f66d042643de72de674f3161d1b794ef
Peter O'Hara Adu <ohara.invent@gmail.com>
2016-11-17 17:31:16 +0000

    Generate controller for Landlords entity
    Generate controller for Landlords using
    `bin/rails generate controller Landlords`

---
+++ app/controllers/landlords_controller.rb
@@ -0,0 +1,50 @@
+class LandlordsController < ApplicationController
+
+	def index
+		@landlords = Landlord.all
+	end
+
+	def show
+		@landlord = Landlord.find(params[:id])
+	end
+
+	def new
+		@landlord = Landlord.new
+	end
+	
+	def edit
+		@landlord = Landlord.find(params[:id])
+	end
+
+	def create
+		@landlord = Landlord.new(landlord_params)
+
+		if @landlord.save
+			redirect_to @landlord
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@landlord = Landlord.find(params[:id])
+		
+		if @landlord.update(landlord_params)
+			redirect_to @landlord
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@landlord = Landlord.find(params[:id])
+		@landlord.destroy
+
+		redirect_to landlords_path
+	end
+
+	private
+		def landlord_params
+			params.require(:landlord).permit(:firstname, :lastname, :phone, :email)
+		end
+end
