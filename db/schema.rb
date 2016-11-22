68ec5b470ef959068c7d90eb2120aafc746cc728
Peter O'Hara Adu <ohara.invent@gmail.com>
2016-11-22 14:44:28 +0000

    Create User model and run it's associated migration
    The `Landlord` and `Tenant` models do not sufficiently represent
    how the SunCity system will be used:
    Users of the system will be able to view and rent accommodations
    on the system as well as rent their spaces out to other users,
    thus acting as both tenants and landlords at the same time.
    
    Having two seperate Rails models for Tenant and Landlord means
    that data for users who are both landlords and tenants will be
    stored in two different database tables, and this defeats the
    DRY principle, as well as creating future complications as to
    where data should be retrieved.
    
    In view of this and other considerations, it is best that users
    be represented by a single `Users` model which will store all info
    for a user whether it is Tenant info or Landlord info.
    
    The Users model is created using:
     `bin/rails generate model User first_name last_name \
      email phone_number`
    after which `bin/rails db:migrate` is run

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121094244) do
+ActiveRecord::Schema.define(version: 20161122144206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,15 @@ ActiveRecord::Schema.define(version: 20161121094244) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "phone_number"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   add_foreign_key "accommodations", "accommodation_types"
